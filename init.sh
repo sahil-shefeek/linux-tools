412fc50830e99c480a31545699077a424e8d83bc
Sahil Shefeek <sahilms345@gmail.com>
2023-07-11 20:31:03 +0530

    added codec support

--- init.sh
+++ init.sh
@@ -6,6 +6,10 @@ echo "Performing system update"
 
 sudo apt update && sudo apt upgrade -y
 
+echo "Adding codecs..."
+
+sudo apt install mint-meta-codecs
+
 echo "Installing gcc..."
 
 sudo apt-get install gcc -y
